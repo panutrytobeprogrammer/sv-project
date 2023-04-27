a476f8308dbd9f3d7d0b16a4bb987ef771b9dc3e
panutrytobeprogrammer <s.panuwatb@gmail.com>
2023-04-27 17:42:31 +0700

    remove secret kry

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i6g8+cs99f=m50(ddq27=e^*z^c4*v4d$uh7l=nvaycb4mq-ak'
+SECRET_KEY = 'XXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
